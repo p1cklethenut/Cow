b4fdd080c8cfdbf21d3d3988e324b083b2e66d32
pickle <picklethenutisthebest@gmail.com>
2024-11-20 10:27:41 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1741,5 +1741,5 @@ if (!DEVMODE && EXTERNAL_URL) {
 setTimeout(mainentry,1000);
 setInterval(() => {
   SAVE_DATA(DATAOBJ, DEVMODE, DATABASE_BACKUP_URL);
-}, 30 * 1000);
+}, 60 * 60 * 1000);
 setInterval(broadcastUUID, 5000);
